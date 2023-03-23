744981ccce4816e3e722b84365b6dc8a2912f70a
Smogg21 <spazzaglia98@gmail.com>
2023-03-22 19:18:24 -0500

    Se agregó el módulo de input

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './Components/Button'
+import { Input } from './Components/Input'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +25,7 @@ function App() {
         </button> */}
 
         <Button onClick={() => {alert('hey')}} value="" size="l">Click Me!</Button>
+        <Input placeholder="Introduce tu juego de interes" value="" size="l" type=""/>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
