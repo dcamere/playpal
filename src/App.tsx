6b2abf781e7deb095b30749dba706b90d1a9f3e0
Diego Cámere Reynoso <dcamere@gmail.com>
2023-03-24 11:52:06 -0500

    Se añade matcher y demás

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,22 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Views/Login/Login';
 import { Matcher } from './Views/Matcher';
+import { Header } from './Components/Header';
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Login/>} />
-        <Route path='/Matcher' element={<Matcher/>} />
-        {/* <Route path='/Profile' element={<Profile/>} />
-        <Route path='/Register' element={<Register/>} /> */}
-      </Routes>
-    </Router>
+    <>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login/>} />
+          <Route path='/Matcher' element={<Matcher/>} />
+          {/* <Route path='/Profile' element={<Profile/>} />
+          <Route path='/Register' element={<Register/>} /> */}
+        </Routes>
+      </Router>
+    </>
   )
 }
 
