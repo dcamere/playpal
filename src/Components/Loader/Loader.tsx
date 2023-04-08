40ba8df30b99b7facd72391b48ef2b4a28d73f41
Martin Rios <martin.rios.reynoso@gmail.com>
2023-04-08 16:12:30 -0500

    Añadí una animacion con un svg

--- src/Components/Loader/Loader.tsx
+++ src/Components/Loader/Loader.tsx
@@ -7,7 +7,10 @@ type LoaderProps<T> = {
 export function Loader<T>({isLoading}:LoaderProps<T>) {
     return (
       <div className={`loader ${isLoading ? "loader--open" : ""}`}>
-        <img src="#" alt="#" />
+        <svg width="100" height="100">
+          <circle cx="50" cy="50" r="40" fill="transparent" stroke="gray" stroke-width="10"  />
+          <circle cx="50" cy="50" r="40" fill="transparent" stroke="white" stroke-width="10" stroke-dasharray="50 200" stroke-linecap="round" />
+        </svg>
       </div>
     );
 }
